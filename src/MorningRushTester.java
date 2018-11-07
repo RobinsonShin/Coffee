cf92481dbc335d8e4dbffe8e2087e8ffd8f40459
Robinson Shin <robinsonshin@ou.edu>
2018-11-07 14:00:27 -0600

    Changed some stuff

--- src/MorningRushTester.java
+++ src/MorningRushTester.java
@@ -19,6 +19,8 @@ public class MorningRushTester
         {
             private int gulps;
             
+            final int initialGulps = 5;
+            
             public DrinkCoffee(int inGulps)
             {
                 gulps = inGulps;
@@ -29,20 +31,22 @@ public class MorningRushTester
                 if(gulps >= 0)
                 {
                     System.out.println(gulps);
-                    --gulps;
                 }
                 else if(gulps == 0)
                 {
                     System.out.println("Go to school");
+                    JOptionPane.showMessageDialog(null, "Make another?");
+                    gulps = initialGulps;
                 }
+                --gulps;
             }
         }
         
         DrinkCoffee dc = new DrinkCoffee(5);
         Timer t = new Timer(DELAY, dc);
         t.start();
-        
         JOptionPane.showMessageDialog(null, "Make another?");
+        System.out.println("Just about to go to school");
         System.exit(0);
     }
 }
